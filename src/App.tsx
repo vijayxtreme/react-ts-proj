beb4c91f7772327305e7012778d4684d36afa586
Vijay Menon <vijaymenonx@gmail.com>
2021-04-05 12:47:33 -0700

    Return type

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 interface AppProps {
   color: string;
 }
 
-function App(props:AppProps) {
+function App(props:AppProps):JSX.Element {
+  let [counter, setCounter] = useState(0)
+
+  let onIncrement = ():void => {
+    setCounter(counter + 1)
+  }
+
+  let onDecrement = ():void => {
+    setCounter(counter - 1)
+  }
+
   return (
-    <div>{props.color}</div>
+    <>
+      <div>{props.color}</div>
+      <div>{counter}</div>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </>
   );
 }
 
