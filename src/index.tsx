2a43ee66e22c2f4002eee9fc1af079ab7e15944f
Vijay Menon <vijaymenonx@gmail.com>
2021-04-05 12:14:01 -0700

    Test ts

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App color={`blue`} />
   </React.StrictMode>,
   document.getElementById('root')
 );
